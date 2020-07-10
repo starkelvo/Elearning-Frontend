7dea101314cb2b5351ec1620c213ef31635936a3
Ezecc <ceze@aust.edu.ng>
2020-07-10 11:36:32 +0100

    abot us page

---
+++ pages/about.vue
@@ -0,0 +1,43 @@
+<template>
+<div id="body">
+    <div ></div>
+      <Navbar />
+    <div>
+       <b-img center src="https://empoweredtech.usc.edu/wp-content/uploads/2017/08/em-2-945x350.jpg" alt="Center image"></b-img>
+    </div>
+    <div id="content" style="margin-left:15%">
+    <h1>About Us</h1>
+    <p>
+        Tutorsng is a revolutionary E-learning platform in Africa. We leverage the best in class teachers and technology solutions to create high-quality, affordable, and accessible education for African students. We have built a platform that helps African students be the very best they can be - an app that allows them to maximize their academic achievement and prepare for a future in various disciplines and subjects. Our team of passionate and talented people has together built a learning experience unprecedented in richness, scope, interactivity, and effectiveness.
+    </p>
+  
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+body{
+    background: #F2F2F2
+}
+.nav{
+    position: absolute;
+width: 1440px;
+height: 390px;
+left: 0px;
+top: 0px;
+
+background: linear-gradient(180deg, rgba(203, 200, 200, 0.5) 0%, rgba(196, 196, 196, 0) 100%);
+}
+#content{
+margin: 5% 15% 10%;
+
+
+
+}
+</style>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,15 +1,9 @@
 <template>
 <div id="body">
-    <div id="nav" style="height: 200px"></div>
-
-    <div>
-       <b-img center src="https://empoweredtech.usc.edu/wp-content/uploads/2017/08/em-2-945x350.jpg" alt="Center image"></b-img>
-    </div>
-    <div id="content" style="margin-left:15%">
-    <h1>About Us</h1>
-    <p>
-        Tutorsng is a revolutionary E-learning platform in Africa. We leverage the best in class teachers and technology solutions to create high-quality, affordable, and accessible education for African students. We have built a platform that helps African students be the very best they can be - an app that allows them to maximize their academic achievement and prepare for a future in various disciplines and subjects. Our team of passionate and talented people has together built a learning experience unprecedented in richness, scope, interactivity, and effectiveness.
-    </p>
+    <div ></div>
+      <Navbar />
+    
+      
   
     </div>
 </div>
