2c48f45872916073d90bb5b689930bb35ac6d7fa
Dministry <dministry356@gmail.com>
2020-07-10 01:13:31 +0100

    updated

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,23 @@
 <template>
-    <div>
-
+    <div class="footer">
+        <label for="logo" class="logo">E-LOGO</label><br>
+        <div class="links">
+        <ul>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Contact us</a></li>
+            <li><a href="#">Live Classes</a></li>
+        </ul>
+        <ul>
+            <li><a href="#">Help</a></li>
+            <li><a href="#">Support</a></li>
+            <li><a href="#">Affiliate</a></li>
+            <li><a href="#">Terms & Conditions</a></li>
+        </ul>
+        <ul>
+            <li><a href="#">Featured courses</a></li>
+            <li><a href="#">Blog</a></li>
+        </ul>
+        </div>
     </div>
 </template>
 
@@ -11,5 +28,42 @@ export default {
 </script>
 
 <style scoped>
-    
+.footer {
+    position: absolute;
+    width: 100%;
+    height: 306px;
+    left: 0px;
+    background: linear-gradient(180deg, rgba(107, 98, 253, 0.43) 0%, rgba(196, 196, 196, 0.31) 100%);
+    }
+label.logo {
+  color: #6b62fd;
+  font-size: 35px;
+  font-family: montserrat;
+  line-height: 80px;
+  padding: 0 100px;
+  font-weight: bold;
+}
+ul {
+  margin-right: 100px;
+}
+ul li {
+  display: block;
+  line-height: 30px;
+}
+ul li a {
+    position: relative;
+    left: 143px;
+
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    /* or 25px */
+
+
+    color: #494949;
+}
+.links{
+    display: inline-flex;
+}
 </style>
