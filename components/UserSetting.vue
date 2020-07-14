a7aab577653614fdc9cb96880da8019aca5ea572
Dministry <dministry356@gmail.com>
2020-07-14 12:33:39 +0100

    userpage

---
+++ components/UserSetting.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div>
+            <ul>
+                <li><a href="">John Doe</a></li>
+                <li><a href="">Change Profile Picture</a></li>
+                <li><a href="">Notification</a></li>
+                <li><a href="">Change password</a></li>
+                <li><a href="">Edit Profile</a></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
